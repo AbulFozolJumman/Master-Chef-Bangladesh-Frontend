44e025fc1ddb956f74dc946cf9a15b3a2761eb7d
AbulFozolJumman <abulfozoljumman@gmail.com>
2023-05-02 02:11:45 +0600

    Home page banner section done

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Banner.css"
+
+const Banner = () => {
+    return (
+        <div id='banner'>
+            <h1 className='text-white text-center p-5 fw-bold'>Welcome to <br /> Master Chef Bangladesh</h1>
+        </div>
+    );
+};
+
+export default Banner;

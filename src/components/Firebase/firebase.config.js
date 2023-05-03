61e49d156ac42c96c0c7a81a8f1aa97763181759
AbulFozolJumman <abulfozoljumman@gmail.com>
2023-05-04 00:37:06 +0600

    Environment variable to hide the Firebase done

--- src/components/Firebase/firebase.config.js
+++ src/components/Firebase/firebase.config.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAjVLICCpDbTNB4pXSWO1iV9KwkqvLpEs0",
-  authDomain: "master-chef-bangladesh.firebaseapp.com",
-  projectId: "master-chef-bangladesh",
-  storageBucket: "master-chef-bangladesh.appspot.com",
-  messagingSenderId: "280077640594",
-  appId: "1:280077640594:web:4e18262015a5e1415fd74b"
-};
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- src/components/Provider/AuthProvider.jsx
+++ src/components/Provider/AuthProvider.jsx
@@ -49,7 +49,7 @@ const AuthProvider = ({children}) => {
 
     useEffect(()=>{
         const unsubscribe = onAuthStateChanged(auth, loggedUser => {
-            console.log('logged in user inside auth state observer', loggedUser)
+            // console.log('logged in user inside auth state observer', loggedUser)
             setUser(loggedUser);
             setLoading(false);
         })
